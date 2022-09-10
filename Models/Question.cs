c80ee241c9d04d6530ca58e40ae7833ab624109c
Romanchik Alkesey <romanchikaleksey@gmail.com>
2022-09-10 14:05:39 +0300

    add test generator models

---
+++ Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace Models
+{
+    public class Question
+    {
+        [XmlAttribute("text")]
+        public string Text { get; set; }
+
+        [XmlElement("answer")]
+        public List<Answer> Answers { get; set; }
+
+        public Question(string text, List<Answer> answers)
+        {
+            Text = text;
+            Answers = answers;
+        }
+    }
+}
